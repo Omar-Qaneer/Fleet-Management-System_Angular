82a204d1750c66aafdbac43966b1b091d3e06090
OMAR <s11925044@stu.najah.edu>
2024-06-01 19:21:43 +0300

    Refactored vehicle table component to use Angular Material for data display.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { VehicleComponent } from './components/vehicle/vehicle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink,VehicleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/vehicle/vehicle.component.ts
+++ src/app/components/vehicle/vehicle.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject,AfterViewInit, ViewChild } from '@angular/core';
 import { VehiclesService } from '../../services/vehicles.service';
 import { Vehicle } from '../../types/vehicle';
 import { Observable } from 'rxjs';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 import { GVAR } from '../../../GVAR';
 
 
 @Component({
   selector: 'app-vehicle',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule,MatButtonModule],
   templateUrl: './vehicle.component.html',
   styleUrl: './vehicle.component.css'
 })
 export class VehicleComponent implements OnInit {
+  displayedColumns: string[] = ['VehicleID', 'VehicleNumber', 'VehicleType', 'LastDirection','LastStatus','LastAddress','LastLatitude','LastLongitude','ShowMore'];
+  ELEMENT_DATA: Vehicle[] = [];
+  dataSource = new MatTableDataSource<Vehicle>(this.ELEMENT_DATA);
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+
   vehicles: Vehicle[]=[];
   isLoading = false;
   error: string | null = null;
@@ -26,6 +36,9 @@ export class VehicleComponent implements OnInit {
         var Gvar = new GVAR();
         Gvar = response;
         this.vehicles = response.DicOfDT["Vehicles"];
+        this.ELEMENT_DATA = response.DicOfDT["Vehicles"];
+        this.dataSource.data = response.DicOfDT["Vehicles"];
+        console.log(response);
         this.isLoading = false;
       },
       error: (error) => {
@@ -33,5 +46,9 @@ export class VehicleComponent implements OnInit {
         this.isLoading = false;
       }
     });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 }
